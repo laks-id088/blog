9ddb1d8e6c388de2495349be265e1b70b41cb963
Navya D <161203295+laks-id088@users.noreply.github.com>
2025-03-15 17:27:07 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4001',
+        target: 'https://mindmosaic-6vbc.onrender.com',
         changeOrigin: true,
         secure: false,
       },
